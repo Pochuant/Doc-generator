3d830b000c230b7fc267281bc262db9e6a19dbd5
pochuan <pochuan.tseng@hylinkgroup.com>
2019-05-15 22:04:50 -0700

    modify conf.py

--- conf.py
+++ conf.py
@@ -31,7 +31,8 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['rst2pdf.pdfbuilder','rinoh.frontend.sphinx']
+pdf_documents = [('index', u'rst2pdf', u'Sample rst2pdf doc', u'Gary')]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -108,19 +109,19 @@ htmlhelp_basename = 'doctestdoc'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+    'papersize': 'letterpaper',
 
     # The font size ('10pt', '11pt' or '12pt').
     #
-    # 'pointsize': '10pt',
+    'pointsize': '10pt',
 
     # Additional stuff for the LaTeX preamble.
     #
-    # 'preamble': '',
+    'preamble': '',
 
     # Latex figure (float) alignment
     #
-    # 'figure_align': 'htbp',
+    'figure_align': 'htbp',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
